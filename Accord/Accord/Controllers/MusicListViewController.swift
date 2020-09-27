90a4fe5148bfcec20e34029e21129c27eece8de1
Neestackich <neestackich@gmail.com>
2020-09-27 16:09:03 +0300

    added database manager methods

--- Accord/Accord/Controllers/MusicListViewController.swift
+++ Accord/Accord/Controllers/MusicListViewController.swift
@@ -7,23 +7,65 @@
 
 import UIKit
 
-class MusicListViewController: UIViewController {
+class MusicListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    
+    // MARK: -Properties
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    private let viewControllerId = "PlayerViewController"
 
+    // var tracksList: [Track] = []
+    
+    // MARK: -Methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        setup()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
     }
-    */
+    
+    override func viewDidDisappear(_ animated: Bool) {
 
+    }
+    
+    func setup() {
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.layer.cornerRadius = 40
+        tableView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMinXMaxYCorner]
+        
+        DatabaseManager.shared.getFirebaseTracks {
+            self.tableView.reloadData()
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return DatabaseManager.shared.tracks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let tableViewCell = tableView.dequeueReusableCell(withIdentifier: "MusicTableViewCell") as! MusicTableViewCell
+        tableViewCell.trackNameLabel.text?.append(DatabaseManager.shared.tracks[indexPath.row].trackName ?? "Unknown")
+        tableViewCell.trackNameLabel.text?.append(" - ")
+        tableViewCell.trackNameLabel.text?.append(DatabaseManager.shared.tracks[indexPath.row].author ?? "Unknown")
+        tableViewCell.trackLengthLabel.text = DatabaseManager.shared.tracks[indexPath.row].length ?? "Unknown"
+        tableViewCell.trackImage.image = UIImage(systemName: "music.note")
+        
+        return tableViewCell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let playerViewController = storyboard?.instantiateViewController(withIdentifier: viewControllerId) as! PlayerViewController
+        playerViewController.modalPresentationStyle = .fullScreen
+        playerViewController.trackName = DatabaseManager.shared.tracks[indexPath.row].trackName ?? "Unknown"
+        playerViewController.author = DatabaseManager.shared.tracks[indexPath.row].author ?? "Unknown"
+        
+        present(playerViewController, animated: true)
+    }
 }
