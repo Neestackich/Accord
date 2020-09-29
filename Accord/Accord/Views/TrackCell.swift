f3cbdb7f785be7a64109f22302929a54b6a99f73
Neestackich <neestackich@gmail.com>
2020-09-29 15:16:10 +0300

    added URLSessionManager methods

--- Accord/Accord/Views/TrackCell.swift
+++ Accord/Accord/Views/TrackCell.swift
@@ -17,18 +17,45 @@ class TrackCell: UITableViewCell {
     @IBOutlet weak var trackImage: UIImageView!
     @IBOutlet weak var downloadButton: UIButton!
     @IBOutlet weak var trackLengthLabel: UILabel!
-    @IBOutlet weak var downloadProgressImage: UIImageView!
     @IBOutlet weak var downloadProgressButton: UIView!
+    @IBOutlet weak var downloadButtonImage: UIImageView!
+    @IBOutlet weak var downloadProgressImage: UIImageView!
     @IBOutlet weak var downloadProgressLine: UIProgressView!
     
+    // var url: String?
+    var track: Track?
+    // var trackIndex: Int?
     var circleLoadingStatus: CAShapeLayer!
-    var url: String?
-    var trackIndex: Int?
+    var downloadStatus: DownloadProgress = .unactiveDownload
+    
+    var delegate: TrackCellDelegate?
+    
     
     // MARK: -Methods
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        
+        setup()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
+    func setup() {
+        let circularPath = UIBezierPath(
+            arcCenter: downloadProgressButton.center,
+            radius: 30,
+            startAngle: 0,
+            endAngle: CGFloat.pi * 2,
+            clockwise: true)
+        
+        circleLoadingStatus = CAShapeLayer()
+        circleLoadingStatus.path = circularPath.cgPath
+        circleLoadingStatus.strokeColor = UIColor.white.cgColor
+        circleLoadingStatus.fillColor = UIColor.clear.cgColor
+        circleLoadingStatus.lineWidth = 5
         
         cellView.layer.cornerRadius = 40
         cellView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMinXMaxYCorner]
@@ -41,36 +68,61 @@ class TrackCell: UITableViewCell {
         
         downloadProgressButton.layer.cornerRadius = 30
         
-        setup()
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-    }
-
-    func setup() {
-        downloadProgressButton.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(downloadTap)))
-        
-        
-        circleLoadingStatus = CAShapeLayer()
-        let circularPath = UIBezierPath(arcCenter: downloadProgressButton.center, radius: 30, startAngle: 0, endAngle: CGFloat.pi * 2, clockwise: true)
-
-        circleLoadingStatus.path = circularPath.cgPath
-        circleLoadingStatus.strokeColor = UIColor.white.cgColor
-        circleLoadingStatus.fillColor = UIColor.clear.cgColor
-        circleLoadingStatus.lineWidth = 5
-        
         cellView.layer.addSublayer(circleLoadingStatus)
-        // downloadProgressButton.layer.addSublayer(circleLoadingStatus)
+        
+        downloadProgressButton.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(downloadTap)))
     }
     
     func updateCell(progress: Float) {
         downloadProgressLine.progress = progress
     }
     
+    func configereCell(track: Track) {
+        self.track = track
+        
+        trackNameLabel.text =
+            (track.trackName ?? "Unknown")
+            + " - "
+            + (track.author ?? "Unknown")
+        trackLengthLabel.text = track.length ?? "Unknown"
+        trackImage.image = UIImage(systemName: "music.note")
+        // updateCell(progress: )
+    }
+    
+    
+    // MARK: -Networking
+    
     @objc func downloadTap() {
-        if let url = url, let trackIndex = trackIndex {
-            URLSessionManager.shared.startDownload(url: url, trackIndex: trackIndex)
+        switch downloadStatus {
+        case .unactiveDownload:
+            downloadStatus = .activeDownload
+            downloadButtonImage.image = UIImage(systemName: "pause")
+            
+            if let track = track {
+                let index = Int(track.index)
+                
+                if let url = track.url {
+                    URLSessionManager.shared.startDownload(url: url, trackIndex: index)
+                }
+            }
+        case .activeDownload:
+            downloadStatus = .pausedDownload
+            downloadButtonImage.image = UIImage(systemName: "arrow.down")
+            
+            if let track = track {
+                if let url = track.url {
+                    URLSessionManager.shared.pauseDownload(url: url)
+                }
+            }
+        case .pausedDownload:
+            downloadStatus = .activeDownload
+            downloadButtonImage.image = UIImage(systemName: "pause")
+            
+            if let track = track {
+                if let url = track.url {
+                    URLSessionManager.shared.resumeDownload(url: url)
+                }
+            }
         }
     }
 }
