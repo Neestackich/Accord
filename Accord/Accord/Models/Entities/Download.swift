8c155a912172e208a843c08d6a776d7cc8679e85
Neestackich <neestackich@gmail.com>
2020-09-29 23:52:21 +0300

    removed and then added again

--- Accord/Accord/Models/Entities/Download.swift
+++ Accord/Accord/Models/Entities/Download.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+
+class Download {
+    var isDownloading = false
+    var progress: Float = 0
+    var resumeData: Data?
+    var task: URLSessionDownloadTask?
+    var trackIndex: Int
+    var url: String
+    var downloaded = false
+  
+    init(url: String, trackIndex: Int) {
+        self.url = url
+        self.trackIndex = trackIndex
+  }
+}
