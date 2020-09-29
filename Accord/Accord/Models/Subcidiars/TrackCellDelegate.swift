8c43602a382d02c364f4b7772ec592c7269e715d
Neestackich <neestackich@gmail.com>
2020-09-29 23:54:48 +0300

    added delegate pattern

--- Accord/Accord/Models/Subcidiars/TrackCellDelegate.swift
+++ Accord/Accord/Models/Subcidiars/TrackCellDelegate.swift
@@ -6,3 +6,9 @@
 //
 
 import Foundation
+
+protocol TrackCellDelegate {
+    func startDownload(cell: TrackCell)
+    func pauseDownload(cell: TrackCell)
+    func resumeDownload(cell: TrackCell)
+}
